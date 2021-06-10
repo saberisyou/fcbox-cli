8f73a8a6d20fe8b577a4d69fe9a36f635466c5fb
乐祥孚（003994） <lexiangfu@fcbox.com>
2021-06-10 19:00:39 +0800

    添加3版本模板

--- bin/app.js
+++ bin/app.js
@@ -4,17 +4,7 @@
 const program = require("commander");
 program.version(require("../package.json").version);
 
-program
-  .command("init <name>")
-  .description("初始化项目")
-  .action((name) => {
-    require("../lib/init")(name);
-  });
-program
-  .command("create <name>")
-  .description("创建项目")
-  .action((name) => {
-    require("../lib/create")(name);
-  });
+program.command("init [name]").description("初始化fcbox脚手架").action(require("../lib/init"));
+program.command("create <name>").description("创建fcbox项目").action(require("../lib/create"));
 
 program.parse(process.argv);
